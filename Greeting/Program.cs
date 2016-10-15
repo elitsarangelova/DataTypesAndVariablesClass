601e014f3a63545c5f88e8a2710c1f302e8c34bf
elitsarangelova <elitsa.rangelova95@gmail.com>
2016-10-15 12:58:08 +0300

    new files

---
+++ Greeting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Hello {name} {lastName}.\r\nYou are {age} years old.");
+        }
+    }
+}
---
+++ RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                int sumLastDigits = 0;
+                int digits = i;
+                while (digits > 0)
+                {
+                    sumLastDigits += digits % 10;
+                    digits = digits / 10;
+                }
+                bool special = (sumLastDigits == 5) || (sumLastDigits == 7) || (sumLastDigits == 11);
+                Console.WriteLine("{0} -> {1}", i, special);
+            }
+        }
+    }
+}
