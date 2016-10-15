d630dd74f39f3e0fd8fe69ed4c8b228f501a0ad8
elitsarangelova <elitsa.rangelova95@gmail.com>
2016-10-15 13:01:11 +0300

    new files

--- SpecialNumbers/Program.cs
+++ SpecialNumbers/Program.cs
@@ -13,14 +13,15 @@ namespace SpecialNumbers
             int num = int.Parse(Console.ReadLine());
             for (int i = 1; i <= num; i++)
             {
-                int lastdigit = num % 10;
-                
-                int sum += lastdigit;
-                sum = sum / 10;
-                Console.WriteLine(lastdigit-i);
-               
-
-               
+                int sumLastDigits = 0;
+                int digits = i;
+                while (digits>0)
+                {
+                    sumLastDigits += digits % 10;
+                    digits = digits / 10;
+                }
+                bool special = (sumLastDigits == 5) || (sumLastDigits == 7) || (sumLastDigits == 11);
+                Console.WriteLine("{0} -> {1}",i,special);
             }
         }
     }
